b0288e029fb8235ecfc35106ce15a711eb613bbe
Jesus Enrique Diaz <A00227255@tec.mx>
2023-09-18 10:42:45 -0600

    fINAL

--- FoodBankApp/src/config/FirebaseConnection.js
+++ FoodBankApp/src/config/FirebaseConnection.js
@@ -1,8 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence, initializeRecaptchaConfig } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { firebase } from "@react-native-firebase/auth";
+import {} from "@react-native-firebase/app-check"
+
+firebase.appCheck().initializeAppCheck;
 
 const firebaseConfig = {
   apiKey: "AIzaSyBi6WlKLtdRi9GS6vjusbwXG2AasyebRA0",
@@ -11,14 +15,19 @@ const firebaseConfig = {
   storageBucket: "bamx-cc64f.appspot.com",
   messagingSenderId: "1043818646012",
   appId: "1:1043818646012:web:c849d93665de5b5f154a79",
-  measurementId: "G-310W5CYEM6"
+  measurementId: "G-310W5CYEM6",
+  forceRecaptchaFlowForTesting: true,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+firebase.appCheck().initializeAppCheck({ provider: rnfbProvider, isTokenAutoRefreshEnabled: true });
+
+
 
 export const db = getFirestore(app);
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+initializeRecaptchaConfig(auth)
 export default app;
