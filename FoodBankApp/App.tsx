e9b6eb2c9c8f64b4c448ae01453b3fae261745c4
Geek-Deer <geekdeer@gmail.com>
2023-10-12 08:04:31 -0600

    Added SplashScreen for android.

--- FoodBankApp/App.tsx
+++ FoodBankApp/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Platform} from 'react-native';
 import AppNavigation from './src/navigation/appNavigation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   
   // <AppNavigation></AppNavigation>
 AsyncStorage.clear();
+
+const hideSplashScreen = () => {
+  SplashScreen.hide();
+}
+
+
+useEffect (() => {
+  hideSplashScreen();
+});
+
 return(
   <AppNavigation></AppNavigation>
   ); 
--- FoodBankApp/android/app/src/main/java/com/foodbankapp/MainApplication.java
+++ FoodBankApp/android/app/src/main/java/com/foodbankapp/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -23,8 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
           return packages;
         }
 
@@ -54,9 +54,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
